bfe976ba871bfce84b6f6a0c39ccbfa33f6d1e82
fosa <fosa@ti04.brasferro.intranet>
2014-11-10 14:45:01 -0400

    add geturl without auth

--- src/tk/egsf/ddns/JSON_helper.java
+++ src/tk/egsf/ddns/JSON_helper.java
@@ -92,6 +92,62 @@ public class JSON_helper {
         return ret;
     }
 
+    public static String getJsonUrl(String URL) {
+        String ret = "";
+
+        String https_url = URL;
+        URL url;
+        try {
+
+            TrustManager[] trustAllCerts = new TrustManager[]{new X509TrustManager() {
+                public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                    return null;
+                }
+
+                @Override
+                public void checkClientTrusted(X509Certificate[] certs, String authType) {
+                }
+
+                @Override
+                public void checkServerTrusted(X509Certificate[] certs, String authType) {
+                }
+            }
+            };
+
+            // Install the all-trusting trust manager
+            SSLContext sc = SSLContext.getInstance("SSL");
+            sc.init(null, trustAllCerts, new java.security.SecureRandom());
+            HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+
+            url = new URL(https_url);
+            HttpsURLConnection con = (HttpsURLConnection) url.openConnection();
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            String input;
+            while ((input = br.readLine()) != null) {
+                ret += input;
+            }
+            br.close();
+
+            System.out.println(ret);
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (ConnectException e) {
+            Logger.getLogger(login.class.getName()).log(Level.SEVERE, null, "Exgotou o tempo de resposta");
+            System.out.println("");
+        } catch (NoSuchAlgorithmException ex) {
+            Logger.getLogger(login.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (KeyManagementException ex) {
+            Logger.getLogger(login.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException e) {
+            e.printStackTrace();
+
+        }
+
+        return ret;
+    }
+
     public static String postToUrl(String URL, String Auth, JSONObject post) {
         String ret = "";
 
@@ -133,7 +189,6 @@ public class JSON_helper {
             System.out.println(post.toString());
 
             byte[] outputBytes = post.toString().getBytes("UTF-8");
-//            byte[] outputBytes = "{'host': 'brasferro'}".getBytes("UTF-8");
             OutputStream os = con.getOutputStream();
             os.write(outputBytes);
             os.close();
